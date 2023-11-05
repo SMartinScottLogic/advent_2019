536d8a9d807709d214d01b1c87fc5eccd2b48226
Simon Martin <havvoric@gmail.com>
2023-11-05 22:00:29 +0000

    Day 7 complete

--- day7/src/intcode.rs
+++ day7/src/intcode.rs
@@ -1,154 +1,223 @@
 use std::collections::VecDeque;
 
-use tracing::debug;
+use tracing::{debug, trace};
 
-pub fn execute(
+#[derive(Debug)]
+pub struct CPU {
+    id: usize,
     ip: usize,
-    mem: &mut [i64],
-    inputs: &mut VecDeque<i64>,
-    outputs: &mut Vec<i64>,
-) -> (bool, usize) {
-    let opcode = mem[ip];
-    let s: Vec<_> = format!("{opcode}").chars().rev().collect();
-    let opcode = s
-        .iter()
-        .take(2)
-        .rev()
-        .collect::<String>()
-        .parse::<i64>()
-        .unwrap();
-    let immediate = s.iter().skip(2).map(|v| v != &'0').collect::<Vec<_>>();
-    debug!(opcode, immediate = debug(&immediate), "t2");
-    // TODO immediate mode extraction
-    match opcode {
-        99 => (true, execute_halt()),
-        1 => (false, execute_add(ip, mem, &immediate)),
-        2 => (false, execute_mult(ip, mem, &immediate)),
-        3 => (false, execute_input(ip, mem, inputs)),
-        4 => (false, execute_output(ip, mem, &immediate, outputs)),
-        5 => (false, execute_jump_if_true(ip, mem, &immediate)),
-        6 => (false, execute_jump_if_false(ip, mem, &immediate)),
-        7 => (false, execute_less_than(ip, mem, &immediate)),
-        8 => (false, execute_equals(ip, mem, &immediate)),
-        _ => panic!("unknown opcode: {opcode}"),
+    mem: Vec<i64>,
+    inputs: VecDeque<i64>,
+    outputs: Vec<i64>,
+    halted: bool,
+}
+
+impl CPU {
+    pub fn new(id: usize, mem: &[i64]) -> Self {
+        Self {
+            id,
+            ip: 0,
+            mem: mem.to_vec(),
+            inputs: VecDeque::new(),
+            outputs: Vec::new(),
+            halted: false,
+        }
     }
 }
 
-fn execute_halt() -> usize {
-    1
-}
+impl CPU {
+    pub fn id(&self) -> usize {
+        self.id
+    }
 
-fn execute_add(ip: usize, mem: &mut [i64], immediate: &[bool]) -> usize {
-    let mut a = mem[ip + 1];
-    let mut b = mem[ip + 2];
-    if !immediate.first().unwrap_or(&false) {
-        a = mem[a as usize];
+    pub fn input(&mut self, value: i64) {
+        self.inputs.push_back(value);
     }
-    if !immediate.get(1).unwrap_or(&false) {
-        b = mem[b as usize];
-    }
-    let addr_r = mem[ip + 3] as usize;
-    let r = a + b;
-    debug!(a, b, addr_r, r, "add");
-    mem[addr_r] = r;
-    ip + 4
-}
 
-fn execute_mult(ip: usize, mem: &mut [i64], immediate: &[bool]) -> usize {
-    let mut a = mem[ip + 1];
-    let mut b = mem[ip + 2];
-    if !immediate.first().unwrap_or(&false) {
-        a = mem[a as usize];
+    pub fn take_output(&mut self) -> Vec<i64> {
+        let mut outputs = Vec::new();
+        outputs.append(&mut self.outputs);
+        outputs
     }
-    if !immediate.get(1).unwrap_or(&false) {
-        b = mem[b as usize];
-    }
-    let addr_r = mem[ip + 3] as usize;
-    let r = a * b;
-    debug!(a, b, addr_r, r, "mult");
-    mem[addr_r] = r;
-    ip + 4
-}
 
-fn execute_input(ip: usize, mem: &mut [i64], inputs: &mut VecDeque<i64>) -> usize {
-    let input = inputs.pop_front().unwrap();
-    let addr = mem[ip + 1] as usize;
-    mem[addr] = input;
-    debug!(addr, "input");
-    ip + 2
-}
+    pub fn has_halted(&self) -> bool {
+        self.halted
+    }
 
-fn execute_output(ip: usize, mem: &mut [i64], immediate: &[bool], outputs: &mut Vec<i64>) -> usize {
-    let mut a = mem[ip + 1];
-    if !immediate.first().unwrap_or(&false) {
-        a = mem[a as usize];
+    pub fn execute(&mut self) -> bool {
+        let opcode = self.mem[self.ip];
+        let s: Vec<_> = format!("{opcode}").chars().rev().collect();
+        let opcode = s
+            .iter()
+            .take(2)
+            .rev()
+            .collect::<String>()
+            .parse::<i64>()
+            .unwrap();
+        let immediate = s.iter().skip(2).map(|v| v != &'0').collect::<Vec<_>>();
+        trace!(opcode, immediate = debug(&immediate), "t2");
+        match opcode {
+            99 => {
+                self.execute_halt();
+                true
+            }
+            1 => {
+                self.execute_add(&immediate);
+                false
+            }
+            2 => {
+                self.execute_mult(&immediate);
+                false
+            }
+            3 => {
+                self.execute_input();
+                false
+            }
+            4 => {
+                self.execute_output(&immediate);
+                false
+            }
+            5 => {
+                self.execute_jump_if_true(&immediate);
+                false
+            }
+            6 => {
+                self.execute_jump_if_false(&immediate);
+                false
+            }
+            7 => {
+                self.execute_less_than(&immediate);
+                false
+            }
+            8 => {
+                self.execute_equals(&immediate);
+                false
+            }
+            _ => panic!("unknown opcode: {opcode}"),
+        }
     }
-    debug!(a, "output");
-    outputs.push(a);
-    ip + 2
-}
 
-fn execute_jump_if_true(ip: usize, mem: &mut [i64], immediate: &[bool]) -> usize {
-    let mut a = mem[ip + 1];
-    if !immediate.first().unwrap_or(&false) {
-        a = mem[a as usize];
+    fn execute_halt(&mut self) {
+        self.halted = true;
+        debug!(id = self.id, cpu = debug(&self), "halt");
     }
-    let mut b = mem[ip + 2];
-    if !immediate.get(1).unwrap_or(&false) {
-        b = mem[b as usize];
-    }
-    if a != 0 {
-        b as usize
-    } else {
-        ip + 3
-    }
-}
 
-fn execute_jump_if_false(ip: usize, mem: &mut [i64], immediate: &[bool]) -> usize {
-    let mut a = mem[ip + 1];
-    if !immediate.first().unwrap_or(&false) {
-        a = mem[a as usize];
+    fn execute_add(&mut self, immediate: &[bool]) {
+        let mut a = self.mem[self.ip + 1];
+        let mut b = self.mem[self.ip + 2];
+        if !immediate.first().unwrap_or(&false) {
+            a = self.mem[a as usize];
+        }
+        if !immediate.get(1).unwrap_or(&false) {
+            b = self.mem[b as usize];
+        }
+        let addr_r = self.mem[self.ip + 3] as usize;
+        let r = a + b;
+        debug!(a, b, addr_r, r, "add");
+        self.mem[addr_r] = r;
+        self.ip += 4;
     }
-    let mut b = mem[ip + 2];
-    if !immediate.get(1).unwrap_or(&false) {
-        b = mem[b as usize];
-    }
-    debug!(a, b, "jf");
-    if a == 0 {
-        b as usize
-    } else {
-        ip + 3
-    }
-}
 
-fn execute_less_than(ip: usize, mem: &mut [i64], immediate: &[bool]) -> usize {
-    let mut a = mem[ip + 1];
-    let mut b = mem[ip + 2];
-    if !immediate.first().unwrap_or(&false) {
-        a = mem[a as usize];
+    fn execute_mult(&mut self, immediate: &[bool]) {
+        let mut a = self.mem[self.ip + 1];
+        let mut b = self.mem[self.ip + 2];
+        if !immediate.first().unwrap_or(&false) {
+            a = self.mem[a as usize];
+        }
+        if !immediate.get(1).unwrap_or(&false) {
+            b = self.mem[b as usize];
+        }
+        let addr_r = self.mem[self.ip + 3] as usize;
+        let r = a * b;
+        debug!(a, b, addr_r, r, "mult");
+        self.mem[addr_r] = r;
+        self.ip += 4;
     }
-    if !immediate.get(1).unwrap_or(&false) {
-        b = mem[b as usize];
-    }
-    let addr_r = mem[ip + 3] as usize;
-    let r = if a < b { 1 } else { 0 };
-    debug!(a, b, addr_r, r, "<");
-    mem[addr_r] = r;
-    ip + 4
-}
 
-fn execute_equals(ip: usize, mem: &mut [i64], immediate: &[bool]) -> usize {
-    let mut a = mem[ip + 1];
-    let mut b = mem[ip + 2];
-    if !immediate.first().unwrap_or(&false) {
-        a = mem[a as usize];
+    fn execute_input(&mut self) {
+        if self.inputs.is_empty() {
+            debug!(cpu = self.id, cpu = debug(&self), "no input");
+            return;
+        }
+        let input = self.inputs.pop_front().unwrap();
+        let addr = self.mem[self.ip + 1] as usize;
+        self.mem[addr] = input;
+        debug!(addr, "input");
+        self.ip += 2;
     }
-    if !immediate.get(1).unwrap_or(&false) {
-        b = *mem.get(b as usize).unwrap_or(&0);
+
+    fn execute_output(&mut self, immediate: &[bool]) {
+        let mut a = self.mem[self.ip + 1];
+        if !immediate.first().unwrap_or(&false) {
+            a = self.mem[a as usize];
+        }
+        debug!(a, "output");
+        self.outputs.push(a);
+        self.ip += 2;
     }
-    let addr_r = mem[ip + 3] as usize;
-    let r = if a == b { 1 } else { 0 };
-    debug!(a, b, addr_r, r, "=");
-    mem[addr_r] = r;
-    ip + 4
+
+    fn execute_jump_if_true(&mut self, immediate: &[bool]) {
+        let mut a = self.mem[self.ip + 1];
+        if !immediate.first().unwrap_or(&false) {
+            a = self.mem[a as usize];
+        }
+        let mut b = self.mem[self.ip + 2];
+        if !immediate.get(1).unwrap_or(&false) {
+            b = self.mem[b as usize];
+        }
+        if a != 0 {
+            self.ip = b as usize;
+        } else {
+            self.ip += 3;
+        }
+    }
+
+    fn execute_jump_if_false(&mut self, immediate: &[bool]) {
+        let mut a = self.mem[self.ip + 1];
+        if !immediate.first().unwrap_or(&false) {
+            a = self.mem[a as usize];
+        }
+        let mut b = self.mem[self.ip + 2];
+        if !immediate.get(1).unwrap_or(&false) {
+            b = self.mem[b as usize];
+        }
+        debug!(a, b, "jf");
+        if a == 0 {
+            self.ip = b as usize;
+        } else {
+            self.ip += 3;
+        }
+    }
+
+    fn execute_less_than(&mut self, immediate: &[bool]) {
+        let mut a = self.mem[self.ip + 1];
+        let mut b = self.mem[self.ip + 2];
+        if !immediate.first().unwrap_or(&false) {
+            a = self.mem[a as usize];
+        }
+        if !immediate.get(1).unwrap_or(&false) {
+            b = self.mem[b as usize];
+        }
+        let addr_r = self.mem[self.ip + 3] as usize;
+        let r = if a < b { 1 } else { 0 };
+        debug!(a, b, addr_r, r, "<");
+        self.mem[addr_r] = r;
+        self.ip += 4;
+    }
+
+    fn execute_equals(&mut self, immediate: &[bool]) {
+        let mut a = self.mem[self.ip + 1];
+        let mut b = self.mem[self.ip + 2];
+        if !immediate.first().unwrap_or(&false) {
+            a = self.mem[a as usize];
+        }
+        if !immediate.get(1).unwrap_or(&false) {
+            b = self.mem[b as usize];
+        }
+        let addr_r = self.mem[self.ip + 3] as usize;
+        let r = if a == b { 1 } else { 0 };
+        debug!(a, b, addr_r, r, "=");
+        self.mem[addr_r] = r;
+        self.ip += 4;
+    }
 }
--- day7/src/lib.rs
+++ day7/src/lib.rs
@@ -1,5 +1,5 @@
 use std::{
-    collections::VecDeque,
+    collections::HashMap,
     io::{BufRead, BufReader},
 };
 
@@ -48,8 +48,32 @@ impl utils::Solution for Solution {
     }
 
     fn answer_part2(&self, _is_full: bool) -> Self::Result {
+        let mut max_output = 0;
+        for a in 5..=9 {
+            for b in 5..=9 {
+                if a != b {
+                    for c in 5..=9 {
+                        if a != c && b != c {
+                            for d in 5..=9 {
+                                if a != d && b != d && c != d {
+                                    for e in 5..=9 {
+                                        if a != e && b != e && c != e && d != e {
+                                            let phases = vec![a, b, c, d, e];
+                                            let output = self.run_trial(&phases);
+                                            if output > max_output {
+                                                max_output = output;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
         // Implement for problem
-        Ok(0)
+        Ok(max_output)
     }
 }
 
@@ -59,26 +83,55 @@ impl Solution {
     }
 
     fn run_trial(&self, phases: &[i64]) -> i64 {
+        let mut thruster_outputs = Vec::new();
         let mut output = 0;
-        for phase in phases {
-            let mut ip = 0;
-            let mem = &mut self.entries.clone()[..];
-            let mut outputs = Vec::new();
-            let mut inputs = VecDeque::new();
-            inputs.push_back(*phase);
-            inputs.push_back(output);
-            loop {
-                let (term, delta) = intcode::execute(ip, mem, &mut inputs, &mut outputs);
-                if term {
-                    break;
-                }
-                ip = delta;
-            }
-            debug!(mem = debug(mem), output = debug(&outputs), "done");
-
-            output = *outputs.iter().next_back().unwrap_or(&-1);
+        let mut cpus = phases
+            .iter()
+            .enumerate()
+            .map(|(id, phase)| {
+                let mut cpu = intcode::CPU::new(id, &self.entries);
+                cpu.input(*phase);
+                cpu
+            })
+            .collect::<Vec<_>>();
+        if let Some(cpu) = cpus.get_mut(0) {
+            cpu.input(0);
         }
-        debug!(phases = debug(phases), output, "trial");
+        loop {
+            for cpu in cpus.iter_mut() {
+                cpu.execute();
+            }
+            // output -> next in chain
+            let mut new_inputs = HashMap::new();
+            for cpu in cpus.iter_mut() {
+                let outputs = cpu.take_output();
+                if !outputs.is_empty() {
+                    if cpu.id() == 4 {
+                        thruster_outputs.append(&mut outputs.clone());
+                    }
+                    new_inputs.insert((cpu.id() + 1) % phases.len(), outputs);
+                }
+            }
+            if !new_inputs.is_empty() {
+                debug!(new_inputs = debug(&new_inputs), "new inputs");
+                for (id, inputs) in new_inputs {
+                    let cpu = cpus.get_mut(id).unwrap();
+                    for input in inputs {
+                        cpu.input(input);
+                    }
+                }
+            }
+            if cpus.iter().all(|cpu| cpu.has_halted()) {
+                break;
+            }
+        }
+        debug!(
+            phases = debug(phases),
+            output,
+            thruster_outputs = debug(&thruster_outputs),
+            "trial"
+        );
+        output = *thruster_outputs.iter().next_back().unwrap_or(&-1);
         output
     }
 }
