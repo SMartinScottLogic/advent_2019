167487e89ea9b73733f658a1e0ccb8e420a05d0c
Simon Martin <havvoric@gmail.com>
2023-11-19 14:51:39 +0000

    Day 14 complete

--- day14/src/lib.rs
+++ day14/src/lib.rs
@@ -4,7 +4,7 @@ use std::{
 };
 
 use regex::Regex;
-use tracing::info;
+use tracing::debug;
 
 pub type ResultType = u64;
 
@@ -18,15 +18,64 @@ impl utils::Solution for Solution {
     fn analyse(&mut self, _is_full: bool) {}
 
     fn answer_part1(&self, _is_full: bool) -> Self::Result {
+        let ore = self.required_ore(1);
+        // Implement for problem
+        Ok(ore)
+    }
+
+    fn answer_part2(&self, _is_full: bool) -> Self::Result {
+        let target_fuel: ResultType = 1_000_000_000_000;
+        let cost_one = self.required_ore(1);
+
+        // left is current max which CAN deliver
+        // right is current min which CAN'T deliver
+        let mut left = target_fuel / cost_one;
+        let mut right = left * 2;
+
+        while right - left > 1 {
+            let mid = (right + left) / 2;
+            let required = self.required_ore(mid);
+            if required < target_fuel {
+                left = mid;
+            } else {
+                right = mid;
+            }
+        }
+        // Implement for problem
+        Ok(left)
+    }
+}
+
+impl Solution {
+    fn add_reaction(&mut self, reaction: Reaction) {
+        self.reactions.push(reaction);
+    }
+
+    fn use_in_hand(reagent: &Reagent, in_hand: &mut HashMap<String, ResultType>) -> ResultType {
+        match in_hand.get_mut(&reagent.name) {
+            None => reagent.count,
+            Some(c) if *c > reagent.count => {
+                *c -= reagent.count;
+                0
+            }
+            Some(c) => {
+                let still_need = reagent.count - *c;
+                *c = 0;
+                still_need
+            }
+        }
+    }
+
+    fn required_ore(&self, desired_fuel: ResultType) -> ResultType {
         let mut ore: ResultType = 0;
         let mut in_hand = HashMap::new();
         let mut required = vec![Reagent {
             name: String::from("FUEL"),
-            count: 1,
+            count: desired_fuel,
         }];
         while let Some(requirement) = required.pop() {
             if requirement.name == "ORE" {
-                ore += requirement.count as u64;
+                ore += requirement.count;
                 continue;
             }
             let required_count = Self::use_in_hand(&requirement, &mut in_hand);
@@ -40,10 +89,11 @@ impl utils::Solution for Solution {
                 .find(|r| r.output.name == requirement.name)
                 .unwrap();
             // How many reactions MUST we have to produce AT LEAST the requirements
-            let multiplier = (required_count as f64 / reaction.output.count as f64).ceil() as usize;
+            let multiplier =
+                (required_count as f64 / reaction.output.count as f64).ceil() as ResultType;
             let remainder = reaction.output.count * multiplier - required_count;
             *in_hand.entry(reaction.output.name.clone()).or_insert(0) += remainder;
-            info!(
+            debug!(
                 requirement = debug(requirement),
                 required_count,
                 reaction = debug(reaction),
@@ -59,34 +109,7 @@ impl utils::Solution for Solution {
                 });
             }
         }
-        // Implement for problem
-        Ok(ore)
-    }
-
-    fn answer_part2(&self, _is_full: bool) -> Self::Result {
-        // Implement for problem
-        Ok(0)
-    }
-}
-
-impl Solution {
-    fn add_reaction(&mut self, reaction: Reaction) {
-        self.reactions.push(reaction);
-    }
-
-    fn use_in_hand(reagent: &Reagent, in_hand: &mut HashMap<String, usize>) -> usize {
-        match in_hand.get_mut(&reagent.name) {
-            None => reagent.count,
-            Some(c) if *c > reagent.count => {
-                *c -= reagent.count;
-                0
-            }
-            Some(c) => {
-                let still_need = reagent.count - *c;
-                *c = 0;
-                still_need
-            }
-        }
+        ore
     }
 }
 
@@ -112,7 +135,7 @@ struct Reaction {
 #[derive(Debug)]
 struct Reagent {
     name: String,
-    count: usize,
+    count: ResultType,
 }
 impl From<String> for Reaction {
     fn from(value: String) -> Self {
